d55d17457648a9324aecafbf5d95083d206c58b7
NeuronsWork <enjey11@gmail.com>
2016-05-17 23:44:40 -0500

    todo completo solo falt contacto

--- assets/js/ualm.js
+++ assets/js/ualm.js
@@ -2,3 +2,11 @@
 $('.carousel').carousel({
      interval: 3000
 });
+
+/* --- MODAL --- */
+$('#Modal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var recipient = button.data('whatever') // Extract info from data-* attributes
+  var modal = $(this)
+  modal.find('.modal-body p span').text('Acabas de Agregar un servicio '+recipient)
+});
