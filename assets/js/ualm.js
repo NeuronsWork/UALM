c80a560921591a9c911fae1dfb74b6690e3bea7d
NeuronsWork <enjey11@gmail.com>
2016-05-18 11:06:04 -0500

    Falta contacto

--- assets/js/ualm.js
+++ assets/js/ualm.js
@@ -10,3 +10,10 @@ $('#Modal').on('show.bs.modal', function (event) {
   var modal = $(this)
   modal.find('.modal-body p span').text('Acabas de Agregar un servicio '+recipient)
 });
+
+$('#gracias').on('show.bs.modal', function (event) {
+  //var button = $(event.relatedTarget) // Button that triggered the modal
+  //var recipient = button.data('whatever') // Extract info from data-* attributes
+  //var modal = $(this)
+  //modal.find('.modal-body p span').text('Acabas de Agregar un servicio '+recipient)
+});
